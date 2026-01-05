aa04597c5e70aa921de6dabd79660a471b86ac15
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2026-01-05 08:37:10 -0500

    Init Commit

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-s!o!l7jbivf$%pz#^o0a$i$#*k5$3qjcts67@sjpzcw$tm27rd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
